86f12746b7ec34185a4d7f14677dda55ff347306
Ben Field <ben.field@mediaclash.co.uk>
2014-11-07 10:14:36 +0000

    Added updated settings - removed rest_framework

--- maplecroft/settings.py
+++ maplecroft/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'south',
     'tweetlocator',
     'kombu.transport.django',
